6f4f234ff72c8a4569697f9097c93470f91e21f9
Jacob Kamble <jacobkamble@gmail.com>
2024-08-05 20:24:12 +0530

    productExceptSelf

--- src/main.ts
+++ src/main.ts
@@ -319,6 +319,26 @@ export function throttle<T extends (...args: any[]) => void>(func: T, limit: num
   };
 }
 
+export const productExceptSelf=(nums: number[]): number[]=> {
+  const length = nums.length;
+  const result: number[] = new Array(length).fill(1);
+
+  let leftProduct = 1;
+  for (let i = 0; i < length; i++) {
+    result[i] = leftProduct;
+    leftProduct *= nums[i];
+  }
+
+  let rightProduct = 1;
+  for (let i = length - 1; i >= 0; i--) {
+    result[i] *= rightProduct;
+    rightProduct *= nums[i];
+  }
+
+  return result;
+}
+
+
 
 
 
