461fdb781bfd75c748e93add6ee9815bf6573e15
Jacob Kamble <jacobkamble@gmail.com>
2024-08-21 14:47:14 +0530

    grouping

--- src/main.ts
+++ src/main.ts
@@ -64,8 +64,6 @@ export const isPrime = (num: number): boolean => {
   return true;
 };
 
-
-
 export const calculatePrimeRange = (num1: number, nu2: number): number[] => {
   let primeRange: number[] = [];
 
@@ -149,7 +147,6 @@ export const data: DataType[] = [
   { parentId: 5, id: 5, name: "Amazon" },
 ];
 
-
 export const findGrouping = (arr: DataType[]): { [key: number]: string[] } => {
   let obj: { [key: number]: string[] } = {};
 
@@ -227,9 +224,11 @@ export const findDuplicateElemnt = (arr: number[]): number[] => {
   return duplicateArr;
 };
 
-export const filterUniqueElement = (arr: number[]): number[] => arr.filter((currEle) => arr.indexOf(currEle) === arr.lastIndexOf(currEle))
+export const filterUniqueElement = (arr: number[]): number[] =>
+  arr.filter((currEle) => arr.indexOf(currEle) === arr.lastIndexOf(currEle));
 
-export const filterDuplicateElement = (arr: number[]): number[] =>arr.filter((currEle) => arr.indexOf(currEle) !== arr.lastIndexOf(currEle));
+export const filterDuplicateElement = (arr: number[]): number[] =>
+  arr.filter((currEle) => arr.indexOf(currEle) !== arr.lastIndexOf(currEle));
 
 export const bubbleSort = (arr: number[]): number[] => {
   for (let i = 0; i < arr.length - 1; i++) {
@@ -260,8 +259,8 @@ export const selectionSort = (arr: number[]): number[] => {
 };
 
 export const findLongestWord = (sentence: string): string => {
-  const words = sentence.split(' ');
-  let longestWord = '';
+  const words = sentence.split(" ");
+  let longestWord = "";
 
   for (const word of words) {
     if (word.length > longestWord.length) {
@@ -285,7 +284,7 @@ export const generateFibonacci = (n: number): number[] => {
   return fibSequence;
 };
 
-export const mergeObjects = (obj1:{}, obj2:{}):{} => ({ ...obj1, ...obj2 });
+export const mergeObjects = (obj1: {}, obj2: {}): {} => ({ ...obj1, ...obj2 });
 
 export const countOccurrences = (substring: string, string: string): number => {
   let count: number = 0;
@@ -297,29 +296,32 @@ export const countOccurrences = (substring: string, string: string): number => {
   }
 
   return count;
-}
+};
 
-export function debounce(func:any, delay:any) {
-  let timer:any;
-  return function(...args:any) {
-      clearTimeout(timer);
-      timer = setTimeout(() => func.apply(this, args), delay);
+export function debounce(func: any, delay: any) {
+  let timer: any;
+  return function (...args: any) {
+    clearTimeout(timer);
+    timer = setTimeout(() => func.apply(this, args), delay);
   };
 }
 
-export function throttle<T extends (...args: any[]) => void>(func: T, limit: number): (...args: Parameters<T>) => void {
+export function throttle<T extends (...args: any[]) => void>(
+  func: T,
+  limit: number
+): (...args: Parameters<T>) => void {
   let inThrottle: boolean;
 
   return (...args: Parameters<T>): void => {
     if (!inThrottle) {
       func(...args);
       inThrottle = true;
-      setTimeout(() => inThrottle = false, limit);
+      setTimeout(() => (inThrottle = false), limit);
     }
   };
 }
 
-export const productExceptSelf=(nums: number[]): number[]=> {
+export const productExceptSelf = (nums: number[]): number[] => {
   const length = nums.length;
   const result: number[] = new Array(length).fill(1);
 
@@ -336,33 +338,80 @@ export const productExceptSelf=(nums: number[]): number[]=> {
   }
 
   return result;
-}
+};
 
 export const isBalanced = (s: string): boolean => {
   const stack: string[] = [];
   const bracketMap: { [key: string]: string } = {
-      ')': '(',
-      '}': '{',
-      ']': '['
+    ")": "(",
+    "}": "{",
+    "]": "[",
   };
 
   for (const char of s) {
-      if (Object.values(bracketMap).includes(char)) {
-          stack.push(char);
-      } else if (Object.keys(bracketMap).includes(char)) {
-          if (stack.length === 0 || stack.pop() !== bracketMap[char]) {
-              return false;
-          }
+    debugger;
+    if (Object.values(bracketMap).includes(char)) {
+      debounce;
+      stack.push(char);
+    } else if (Object.keys(bracketMap).includes(char)) {
+      debugger;
+      if (stack.length === 0 || stack.pop() !== bracketMap[char]) {
+        debugger;
+        return false;
       }
+    }
   }
 
+  debugger;
   return stack.length === 0;
 };
 
+export const numberPalinrone=(num:number):boolean=>{
 
+  let temp=num;
+  let reverseNum=0;
 
+  while(num>0){
+    let lastDigit=num%10;
+    reverseNum=(reverseNum*10)+lastDigit
+    num=num/10
+  }
+ console.log(reverseNum)
+  return reverseNum===temp
+}
 
+export const transformData=(arr:any):any=>{
 
+  const groupMap:any={};
+  arr.for(({userId,groupIds}:any)=>{
+    groupIds.forEach((groupId:any)=>{
+      if(!groupMap[groupId]){
+        groupMap[groupId]=[]
+      }
+      groupMap[groupId].push(userId)
+    })
+  })
+
+  console.log(groupMap)
+
+  return []
+}
+
+ const input = [
+      { userId: 'user0', groupIds: [ 'groupA', 'groupB', 'groupC' ] },
+      { userId: 'user1', groupIds: [ 'groupB', 'groupC' ] },
+      { userId: 'user2', groupIds: [ 'groupA', 'groupB' ] },
+      ];
+  
+      const output = [
+          { groupId: 'groupA', userIds: [ 'user0', 'user2' ] },
+          { groupId: 'groupB', userIds: [ 'user0', 'user1', 'user2' ] },
+          { groupId: 'groupC', userIds: [ 'user0', 'user1' ] },
+          ];"
+
+console.log(isBalanced("()[]{}"));  // Output: true
+console.log(isBalanced("[{()}]"));  // Output: true
+console.log(isBalanced("(]"));      // Output: false
 
 // export const arr = [1, 5, 2, 1, 1, 2, 3, 3, 4, 3, 8, 5, 6, 7, -99, -4, 2];
 // Output: { '1': [ 'TCS', 'Infosys' ], '5': [ 'Wipro', 'HCL', 'Amazon' ] }"
